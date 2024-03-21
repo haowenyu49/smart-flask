0881b9e35316c4c21c6376c427498d05cb1c2ea4
Soroush Mirzaee <DeltaTheM2@users.noreply.github.com>
2024-03-20 23:32:12 -0700

    Dependencies and style changes

--- lib/components/homepage.dart
+++ lib/components/homepage.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:smartflask/components/ArcProgressIndicator.dart';
 import 'package:smartflask/main.dart';
 import 'package:smartflask/components/pie_chart.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -127,45 +129,173 @@ class _HomePageState extends State<HomePage> {
   }
 
 }
+class DateDisplay extends StatelessWidget {
+  final DateTime date;
+
+  DateDisplay({Key? key, required this.date}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String dayNumber = DateFormat('d').format(date); // Day number, e.g., "23"
+    String dayName = DateFormat('E').format(date); // Day name abbreviation, e.g., "Mon"
+
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8), // Reduced padding
+      decoration: BoxDecoration(
+        color: Colors.blue[50], // Change to desired background color
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(
+            dayNumber,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16, // Reduced font size if necessary
+              color: Colors.blue, // Change to desired text color
+            ),
+          ),
+          Text(
+            dayName,
+            style: TextStyle(
+              fontSize: 12, // Reduced font size if necessary
+              color: Colors.grey[600], // Change to desired text color
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class BottomSheetClass extends StatelessWidget {
   const BottomSheetClass({super.key});
+
+  Future<List<Map<String, dynamic>>> fetchLast7DaysData() async {
+    final now = DateTime.now();
+    final sevenDaysAgo = now.subtract(Duration(days: 7));
+    final userUID = 'YOUR_USER_UID'; // Replace with actual user UID.
+
+    // Convert to Unix timestamp for comparison.
+    final int sevenDaysAgoUnix = sevenDaysAgo.millisecondsSinceEpoch ~/ 1000;
+
+    final querySnapshot = await FirebaseFirestore.instance
+        .collection('water_consumption')
+        .where('userId', isEqualTo: userUID)
+        .where('timestamp', isGreaterThanOrEqualTo: sevenDaysAgoUnix)
+        .orderBy('timestamp', descending: true)
+        .get();
+
+    return querySnapshot.docs
+        .map((doc) => {
+      ...doc.data() as Map<String, dynamic>,
+      'date': DateTime.fromMillisecondsSinceEpoch(doc['timestamp'] * 1000)
+    })
+        .toList();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: TextButton(onPressed: ()
-          {
-            showModalBottomSheet<void>(context: context,
-                builder: (BuildContext context) {
-              return Container(
-                height: 600,
-                color: Colors.white,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Row(
+        child: TextButton(
+            onPressed: () {
+              showModalBottomSheet<void>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Container(
+                        height: 600,
+                        color: Colors.white,
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              SizedBox(height: 10),
+                              Container(
+                                height: 60,
+                                child: Center(
+                                  child: SingleChildScrollView(
+                                    scrollDirection: Axis.horizontal,
+                                    child: ConstrainedBox(
+                                      constraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width),
+                                      child: IntrinsicWidth(
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.center, // Center the children
+                                          children: List<Widget>.generate(7, (index) {
+                                            final date = DateTime.now().subtract(Duration(days: index));
+                                            return Padding(
+                                              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                                              child: DateDisplay(date: date),
+                                            );
+                                          }),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
 
-                        ),
-                        const Text('Hello World!'),
-                        TextButton(onPressed: () => Navigator.pop(context), child: const Text('Close'))
-                      ],
-                  ),
-                )
-              );
-                });
-          },
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[const Text('Amount of water this week',
-  style: TextStyle(fontSize: 28, color:Colors.blue),),
-        Icon(Icons.arrow_forward, color: Colors.blue,),
-      ])
-      ));
+                              SizedBox(height: 10),
+                          Expanded(
+                            child: FutureBuilder<List<Map<String, dynamic>>>(
+                              future: fetchLast7DaysData(),
+                              builder: (context, snapshot) {
+                                if (snapshot.connectionState == ConnectionState.waiting) {
+                                  return CircularProgressIndicator();
+                                }
+                                if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                                  return Text('No data available');
+                                }
+                                final data = snapshot.data!;
+                                // Your logic here to display the fetched data
+                                // For simplicity, this example just lists the amounts
+                                return ListView.builder(
+                                  itemCount: data.length,
+                                  itemBuilder: (context, index) {
+                                    final dayData = data[index];
+                                    return ListTile(
+                                      title: Text(DateFormat('MM/dd').format(dayData['date'])),
+                                      subtitle: Text('${dayData['amount']} ml'),
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                        ]));
+                  });
+            },
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: const [
+              Text(
+                'Amount of water this week',
+                style: TextStyle(fontSize: 28, color: Colors.blue),
+              ),
+              Icon(Icons.arrow_forward, color: Colors.blue),
+            ])));
   }
 }
+
+
 //
 // Text('Amount of water this week',
 // style: TextStyle(fontSize: 28,color:Colors.blue),),
 // Icon(Icons.arrow_forward, color: Colors.blue,),
+
+
+Future<List<Map<String, dynamic>>> fetchLast7DaysData() async {
+  final userUID = 'YOUR_USER_UID';
+  final now = DateTime.now();
+  final sevenDaysAgo = now.subtract(Duration(days: 7));
+
+  final querySnapshot = await FirebaseFirestore.instance
+      .collection('water_consumption')
+      .where('userId', isEqualTo: userUID)
+      .where('date', isGreaterThanOrEqualTo: sevenDaysAgo)
+      .orderBy('date', descending: true)
+      .limit(7)
+      .get();
+
+  return querySnapshot.docs
+      .map((doc) => doc.data() as Map<String, dynamic>)
+      .toList();
+}
